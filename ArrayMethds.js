47f7b82bacb29eacec26b8e348859cddbfc7695d
Param Raval <137318998+Param134@users.noreply.github.com>
2023-12-06 10:57:24 +0000

    third commit

--- ArrayMethds.js
+++ ArrayMethds.js
@@ -6,5 +6,31 @@ console.log(numbers, doubleNum);
 let oddNum = numbers.filter((num) => num % 2 != 0)
 console.log(numbers, oddNum);
 
-let arraySum = numbers.reduce((total, num) => total + num);
+let arraySum = numbers.reduce((total, num) => {
+    return total + num;
+}, 0);
 console.log(numbers, arraySum);
+
+//Flattening an Array of Arrays
+
+const nestedArray = [[1, 2], [3, 4], [5, 6]];
+
+const flattenedArray = nestedArray.reduce((accumulator, currentValue) => {
+    return accumulator.concat(currentValue);
+}, []);
+
+console.log(flattenedArray); // Output: [1, 2, 3, 4, 5, 6]
+
+
+//Counting Occurrences of Array Elements:
+
+const names = ['Alice', 'Bob', 'Charlie', 'Alice', 'David', 'Alice'];
+
+const nameOccurrences = names.reduce((accumulator, currentValue) => {
+    accumulator[currentValue] = (accumulator[currentValue] || 0) + 1;
+    return accumulator;
+}, {});
+
+console.log(nameOccurrences);
+// Output: { Alice: 3, Bob: 1, Charlie: 1, David: 1 }
+
---
+++ EventDelegation.js
@@ -0,0 +1,13 @@
+// <!-- HTML -->
+// <ul id="myList">
+//     <li>Item 1</li>
+//     <li>Item 2</li>
+//     <li>Item 3</li>
+// </ul>
+
+// <!-- JavaScript -->
+// document.getElementById("myList").addEventListener("click", function(event) {
+//     if (event.target.tagName === "LI") {
+//         console.log("Clicked on:", event.target.textContent);
+//     }
+// });
